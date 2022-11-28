5c48197359ec517da241d1c4cf678bdc049f17e6
wilson <Yinxinxin@UNIVERSEACTION.com>
2022-11-28 16:03:34 +0800

    feat: add webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,10 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-    clean: true,
+    // clean: true,
+  },
+  devServer: {
+    static: './dist',
   },
   module: {
     rules: [
