b0435d6ee7c6617dd41acbb39df5c83ec6ae6c53
wilson <Yinxinxin@UNIVERSEACTION.com>
2022-12-02 11:47:09 +0800

    feat: update gh-deplpy.sh

--- README.md
+++ README.md
@@ -2,10 +2,6 @@
 
 Traditional Chinese and Simplified Chinese
 
-## dist to git page
-
-`https://gist.github.com/cobyism/4730490`
-
 ## push to github page
 
 ```node
--- gh-deploy.sh
+++ gh-deploy.sh
@@ -1,8 +1,21 @@
-#!/bin/sh
+# yarn run publish
+
+#!/usr/bin/env sh
+
 if [ -z "$1" ]
 then
   echo "Which folder do you want to deploy to GitHub Pages?"
   exit 1
 fi
 
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+yarn build
+git add .
+git commit -m'publish gh-pages'
+
+# https://gist.github.com/cobyism/4730490
+# 发布到子分支 
 git subtree push --prefix $1 github gh-pages
